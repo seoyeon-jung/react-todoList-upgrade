9198cdedd5adb3fcefbebfe1db3b52d5c6cc8b4a
seoyeon-jung <jungluchia1204@gmail.com>
2022-12-13 17:47:47 +0900

    chore: 필요한 setting 완료
    style-componetns / redux / react-router-dom 설치 완료

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import store from "./redux/config/configStore";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // App을 Provider로 감싸주고, configStore에서 export한 store을 넣어주기
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
