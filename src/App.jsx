894fa0a87c9202d56ba0470e5342dd17a9ebb72e
seoyeon-jung <jungluchia1204@gmail.com>
2022-12-19 10:37:49 +0900

    toastcontainer 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import Router from "./shared/Router";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <ToastContainer position={"top-center"} />
+    </>
+  );
 }
 
 export default App;
--- src/components/features/TodoInput.jsx
+++ src/components/features/TodoInput.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../../redux/modules/Todos";
 import { v4 as uuidv4 } from "uuid";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const InputBox = styled.form`
@@ -50,15 +50,15 @@ const TodoInput = () => {
     setTodo({ ...todo, [name]: value });
   };
 
-  // toast
-  // const notify = () => toast("Wow so easy!");
-
   // onSubmit
   const onSubmit = (e) => {
     e.preventDefault(); // 새로고침 방지
 
     // 입력칸 공백 방지
     if (!todo.text || !todo.title) {
+      // app.js 에 <ToastContainer /> 추가
+      // 최상위 컴포넌트에 존재하므로 하위 컴포넌트 어디에서나 사용 가능
+      // => 최적화에 좋다 (나중에 기획 의도 변경되어도 쉽게 사용 가능)
       toast.warning("제목과 내용 모두 입력해주세요!");
       return;
     }
@@ -92,7 +92,7 @@ const TodoInput = () => {
       <InputBtn type="submit" onClick={onSubmit}>
         ➕
       </InputBtn>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
     </InputBox>
   );
 };
